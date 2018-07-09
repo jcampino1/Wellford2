61a71e44ca38004ef6c42c0da105fc2f3009f71b
matiaseche <mgecheverria@uc.cl>
2018-07-08 20:06:33 -0400

    Comentarios funciones

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,11 @@ class Test < ApplicationRecord
 
 	validates :diametro_rodete, presence: true
 
+
   def self.import(pump, file)
+    """
+    Lee el archivo y crea los tests correspondientes a la bomba seleccionada
+    """
     CSV.foreach(file.path, headers: :true) do |row|
       pump.tests.create!(row.to_hash)
     end
