80ea4c3d1d177f0f9e33977811b76fbc459a79b9
Joseca <jcampino1@uc.cl>
2018-07-10 18:36:03 -0400

    Funciona grafico para demo

--- app/assets/javascripts/pumps.js
+++ app/assets/javascripts/pumps.js
@@ -1,16 +1,5 @@
-<h1>Analisis informacion ingresada</h1>
-
-<script src="http://code.highcharts.com/highcharts.js"></script>
-<script src="http://code.highcharts.com/modules/exporting.js"></script>
-
-<script src="//rawgithub.com/phpepe/highcharts-regression/master/highcharts-regression.js?8">
-
-
-</script>
-<div id="container" style="width: 500px; height: 500px; margin: 0 auto"></div>
-
-<script type="text/javascript">
- Highcharts.chart('container', {
+$(function() {
+  $('#container2').highcharts({
     chart: {
       type: 'scatter',
       zoomType: 'xy'
@@ -19,7 +8,7 @@
       text: 'Evaluacion de puntos'
     },
     subtitle: {
-      text: ''
+      text: 'Source: Heinz  2003'
     },
     xAxis: {
       title: {
@@ -65,7 +54,7 @@
         },
         tooltip: {
           headerFormat: '<b>{series.name}</b><br>',
-          pointFormat: '{point.x} cm, {point.y} kg'
+          pointFormat: '{point.x} m3, {point.y} m'
         }
       }
     },
@@ -76,16 +65,19 @@
         color: 'rgba(223, 183, 83, .9)',
         dashStyle: 'dash'
       },
-      name: 'Rodete 214',
+      name: 'Test input',
       color: 'rgba(223, 83, 83, .5)',
-      data: [                
-      	<%@curva_h2.each do |d|%>
-      		<%=raw d %>,
-      	<%end%>                   
- 	  ]
+      data: [
+        [38.996, 45.675],
+        [34.250, 52.025],
+        [29.728, 55.822],
+        [21.589, 61.595],
+        [12.321, 64.756],
+        [1.621, 66.878],
+        
+       
+      ]
+
     }]
   });
-
-</script>
-
-
+});
