28bc8db2a4060896cec2ec232ca00566d8d876f2
matiaseche <mgecheverria@uc.cl>
2018-08-01 17:37:40 -0400

    Admin y solicitudes

--- app/helpers/pumps_helper.rb
+++ app/helpers/pumps_helper.rb
@@ -1,2 +1,12 @@
 module PumpsHelper
+  def ya_postulo(user_id)
+    Request.all.each do |request|
+      if (request.user_id == user_id)
+        return true
+      else
+        return false
+      end
+    end
+    false
+  end
 end
